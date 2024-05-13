1895c159b123f840d05a8d0c177b956fe12db258
Erik Natanael Gustafsson <erik@eriknatanael.com>
2024-05-13 21:53:45 +0200

    Start on SPI receive

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # daisy_receive_spi
+
+## Set up daisy libs
+
+```
+git submodule update --init --recursive
+echo "building DaisySP . . ."
+cd "DaisySP" ; make clean ; make | grep "warning:r\|error" ;
+echo "done."
+
+cd ../libDaisy
+echo "building libDaisy . . ."
+make clean ; make | grep "warning:r\|error" ;
+echo "done."
+```
+
+## Building
+
+cd into the directory of the program you want to build and run
+
+```
+make clean; make
+```
+
+## Uploading
+
+Put Daisy into DFU mode by pressing BOOT, then RESET, then letting go of RESET and then of BOOT.
+
+```
+make program-dfu
+```
+
+Or build and upload in one:
+
+```
+make clean; make; make program-dfu
+```
+
+
+## Docs
+
+https://electro-smith.github.io/libDaisy/md_doc_2md_2__a8___getting-_started-_s_p_i.html
+
+https://github.com/electro-smith/libDaisy/tree/master/examples/SPI
+
+https://forum.electro-smith.com/t/spi-examples/4973
---
+++ receive_harmonics/receive_harmonics.cpp
@@ -0,0 +1,82 @@
+#include "daisy_patch.h"
+#include "daisysp.h"
+#include <string>
+
+using namespace daisy;
+using namespace daisysp;
+
+static DaisySeed hw;
+Oscillator osc;
+uint8_t harmonic = 0;
+float root_freq = 110.0;
+
+static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
+                          AudioHandle::InterleavingOutputBuffer out,
+                          size_t size) {
+  float sig, freq, amp;
+  // size_t wave = 1;
+
+  for (size_t i = 0; i < size; i++) {
+    // Read Knobs
+    freq = root_freq * (harmonic + 1);
+    amp = 0.3;
+    // Set osc params
+    osc.SetFreq(freq);
+    // osc.SetWaveform(wave);
+    osc.SetAmp(amp);
+    //.Process
+    sig = osc.Process();
+    // Assign Synthesized Waveform to all four outputs.
+    for (size_t chn = 0; chn < 4; chn++) {
+      out[chn][i] = sig;
+    }
+  }
+}
+
+int main(void) {
+  // Init the seed and so on...
+  // initialize seed hardware and daisysp modules
+  float sample_rate;
+  hw.Configure();
+  hw.Init();
+  hw.SetAudioBlockSize(4);
+  sample_rate = hw.AudioSampleRate();
+
+  // set parameters for sine oscillator object
+  osc.Init(sample_rate);
+  osc.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW);
+  osc.SetFreq(100);
+  osc.SetAmp(0.25);
+
+  // SpiHandle object and Spi Configuration object
+  SpiHandle spi_handle;
+  SpiHandle::Config spi_conf;
+
+  // Set some configurations
+  spi_conf.periph = SpiHandle::Config::Peripheral::SPI_1;
+  spi_conf.mode = SpiHandle::Config::Mode::SLAVE;
+  spi_conf.direction = SpiHandle::Config::Direction::TWO_LINES_RX_ONLY;
+  spi_conf.nss = SpiHandle::Config::NSS::HARD_INPUT;
+
+  spi_conf.pin_config.sclk = Pin(PORTG, 11);
+  spi_conf.pin_config.miso =
+      Pin(PORTB, 4); // won't use this, but here it is anyways
+  spi_conf.pin_config.mosi = Pin(PORTB, 5);
+  spi_conf.pin_config.nss = Pin(PORTG, 10);
+
+  // Initialize the handle using our configuration
+  spi_handle.Init(spi_conf);
+
+  // any final initialization...
+
+  // start callback
+  hw.StartAudio(AudioCallback);
+
+  // infinite loop
+  while (1) {
+    // Blocking read of 4 bytes from SPI
+    uint8_t buffer[4];
+    spi_handle.BlockingReceive(&buffer, 1);
+    harmonic = buffer[0];
+  }
+}
